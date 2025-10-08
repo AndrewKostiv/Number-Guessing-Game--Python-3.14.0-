a9154fd7ed94dec9ff219c42525d62b12f4e016f
AndrewKostiv <AndrewKostiv1@gmail.com>
2025-10-08 01:37:42 -0400

    Finalized game and added readme

---
+++ Readme.md
@@ -0,0 +1,20 @@
+## Number Guessing Game (Python)
+
+---
+
+A simple interactive number guessing game implemented in Python. The program generates a random number between 1 and 100, and the player tries to guess it. The game provides hints, tracks attempts, and allows quitting anytime.
+
+# Features
+
+- Generates a random number between 1 and 100.
+- Provides feedback if your guess is too high, too low, or very close.
+- Counts the number of attempts taken to guess correctly.
+- Handles invalid input gracefully.
+- Allows quitting the game by typing "q".
+
+# How to Run
+
+- Make sure Python 3.x is installed.
+```bash
+git clone https://github.com/AndrewKostiv/Number-Guessing-Game--Python-3.14.0-
+py main
--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import random
-randNum = random.randint(1,100)
-guess:int = 0
-count:int = 0
 
-while guess != randNum:
-    guess = int(input("Guess a number: "))
-    count += 1
-    if guess > randNum:
-        print(guess, "is too high")
-    elif guess < randNum:
-        print(guess, "is too low")
+def runGame():
+    randNum = random.randint(1,100)
+    guess:int = 0
+    count:int = 0
+
+    while guess != randNum:
+        try:
+            guess = input("Guess a number (or type \"q\" to quit): ")
+            if guess.lower() == 'q':
+                print("The correct answer was", randNum)
+                break
+            else: 
+                guess = int(guess)
+        except ValueError:
+            print("Incorrect input, type a number")
+            continue
+
+        count += 1
+
+        if (abs(guess - randNum) <= 5 and guess != randNum):
+            print("You are very close!")
+        if guess > randNum:
+            print(guess, "is too high")
+        elif guess < randNum:
+            print(guess, "is too low")
+        else:
+            print(guess, "is correct! You took",count,"attempts")
+
+runGame()
+guess = ''
+while guess != 'n':
+    try:
+        guess = input("Would you like to play again? (y/n)")
+    except ValueError:
+        print('Invalid input, type either "y" or "n"')
+    if guess.lower() == 'y':
+        runGame()
+    elif guess.lower() == 'n':
+        break
     else:
-        print(guess, "is correct! You took",count,"attempts")
+        print('Invalid input, type either "y" or "n"')
+
+
